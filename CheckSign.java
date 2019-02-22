c1b8f34c334a6948f5c2455f400c32afc113ffe9
DHARANIBABU-G <47512865+DHARANIBABU-G@users.noreply.github.com>
2019-02-22 11:44:35 +0530

    Update CheckSign.java

--- CheckSign.java
+++ CheckSign.java
@@ -3,13 +3,13 @@ class CheckSign{
 public static void main(String[] qwe){
 Scanner scan=new Scanner(System.in);
 int a=scan.nextInt();
-if(a<0){
-System.out.println("Negative");
+if(a<0){     //check the negativity
+System.out.println("Negative"); 
 }
-else if(a==0){
+else if(a==0){   //check the zero
 System.out.println("Zero");
 }
-else{
+else{            //check positivity
 System.out.println("Positive");
 }
 }
